a9f1e1b413af16e60534a152a7332d80e5be9631
Kevin Nzeng <kevin.nzeng@energyweb.org>
2022-02-12 15:13:42 +0100

    Delete IMetadata.sol

--- day01/ex01/solution/Interfaces/IMetadata.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: UNLICENCED
-pragma solidity ^0.8.0;
-
-interface IMetadata {
-    function getContractName() external view returns(string memory);
-    function setContractName(string calldata _newContractName) external;
-}
